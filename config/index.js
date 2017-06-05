fbb44bc943ab076f5071547f2cc760fbbcbabe2a
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-06-05 11:28:14 -0700

    Add todoModel.js. Create Schema.

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ var config = require('./config');
 
 module.exports = {
   getDbConnectionString() {
-    return `mongodb://${config.uname}:${config.pwd}@ds161121.mlab.com:61121/bd_todo`
+    return `mongodb://${config.uname}:${config.pwd}@ds161121.mlab.com:61121/bd_todo`;
   }
 }
 
---
+++ models/todoModel.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var config = require('./config');
+
+var Schema = mongoose.Schema;
+
+var todoSchema = new Schema({
+  username: String,
+  todo: String,
+  isDone: Boolean,
+  hasAttachment: Boolean
+});
+
+// create model based on todoSchema
+var Todos = mongoose.model('Todos', todoSchema);
+
+// export model
+module.exports = Todos;
